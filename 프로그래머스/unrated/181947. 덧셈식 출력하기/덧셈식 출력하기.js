bb105afdae1eacf150f48b341bc9574f2c8bbbb1
hapl197 <116621441+neix604@users.noreply.github.com>
2023-07-08 23:54:48 +0900

    [unrated] Title: 덧셈식 출력하기, Time: 50.19 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+let sum = 0;
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    sum = Number(input[0]) + Number(input[1]);
+    console.log(Number(input[0]) + " + " + Number(input[1]) + " = " + sum);
+});
