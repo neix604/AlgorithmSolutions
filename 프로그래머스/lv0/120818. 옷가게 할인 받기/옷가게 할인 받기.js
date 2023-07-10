5c455a4a87a4c3685aeb75fc729c8e18ae6317cc
hapl197 <116621441+neix604@users.noreply.github.com>
2023-07-10 23:45:32 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,3 +1,4 @@
 function solution(price) {
-    return price >= 500000 ? ~~[price * 0.8] : price >= 300000 ? ~~[price * 0.9] : price >= 100000 ? ~~[price * 0.95] : price;
+    let answer = price >= 500000 ? price * 0.8 : price >= 300000 ? price * 0.9 : price >= 100000 ? price * 0.95 : price;
+    return ~~[answer];
 }
