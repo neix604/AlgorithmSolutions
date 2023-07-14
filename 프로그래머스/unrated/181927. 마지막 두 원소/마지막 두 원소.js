92a890c739b50abb8d94b4fa7ed1b263af9ab60d
neix604 <116621441+neix604@users.noreply.github.com>
2023-07-14 23:45:27 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,5 +1,4 @@
 function solution(num_list) {
-    const i = num_list.length - 1;
-    num_list.push(num_list[i] > num_list[i - 1] ? num_list[i] - num_list[i - 1] : num_list[i] * 2)
-    return num_list;
+    const [a, b] = [...num_list].reverse();
+    return [...num_list, a > b ? a - b : a * 2];
 }
