b8898a059ca0b3acfc64b326c4986573b3ceba5c
neix604 <116621441+neix604@users.noreply.github.com>
2023-07-30 00:01:30 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    let answer = [];
+    let arr = my_string.split(' ');
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] !== '') {
+            answer.push(arr[i]);
+        } else {
+            continue;
+        }
+    }
+    return answer;
+}
