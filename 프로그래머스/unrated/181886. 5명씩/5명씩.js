d972cefd5e4bbd84b856c71e90b028e3668dd5ff
neix604 <116621441+neix604@users.noreply.github.com>
2023-08-06 22:53:26 +0900

    [unrated] Title: 5명씩, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.js
@@ -0,0 +1,9 @@
+function solution(names) {
+    let answer = [];
+    for(let i = 0; i < names.length; i++) {
+        if((i + 1) % 5 === 1) {
+            answer.push(names[i]);
+        }
+    }
+    return answer;
+}
